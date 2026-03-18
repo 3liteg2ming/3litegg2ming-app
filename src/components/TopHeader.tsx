721e095c8b6d0e9ae6e306aae9265ed1b6a79a62
3liteg2ming <galezach@gmail.com>
2026-03-18 13:33:52 +1100

    Stabilize mobile polish pass

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -9,29 +9,20 @@ export default function TopHeader() {
       <div className="egTopHeader__backdrop" aria-hidden="true" />
 
       <div className="egTopHeader__inner">
-        <div className="egTopHeader__left">
-          <div className="egTopHeader__labelStack">
-            <span className="egTopHeader__labelSecondary egTopHeader__labelLine1">AFL 26</span>
-            <span className="egTopHeader__labelSecondary egTopHeader__labelLine2">SEASON 2</span>
+        <div className="egTopHeader__logoWrapper" aria-hidden="true">
+          <div className="egTopHeader__brand">
+            <span className="egTopHeader__brandCrop">
+              <img
+                className="egTopHeader__brandLogo"
+                src={assetUrl('elite-gaming-logo.png')}
+                alt=""
+                loading="eager"
+              />
+            </span>
           </div>
         </div>
 
-        <div className="egTopHeader__center">
-          <div className="egTopHeader__logoWrapper" aria-hidden="true">
-            <div className="egTopHeader__brand">
-              <span className="egTopHeader__brandCrop">
-                <img
-                  className="egTopHeader__brandLogo"
-                  src={assetUrl('elite-gaming-logo.png')}
-                  alt=""
-                  loading="eager"
-                />
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div className="egTopHeader__right">
+        <div className="egTopHeader__authContainer">
           <HeaderAuthPill />
         </div>
       </div>
